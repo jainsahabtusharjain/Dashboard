aa223733f5507766f1a362b5e2cab5fbbc4f7c16
jainsahabtusharjain <tusharjain9929@gmail.com>
2024-11-24 17:51:41 +0400

    new changes in  main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import App from './App.tsx'
-import App from "./App.js"; // or './App.js' if applicable
+import App from "./App.tsx";
+// import App from "./App.js"; // or './App.js' if applicable
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
